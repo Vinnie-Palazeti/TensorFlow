2dfc2b6d725c7c67a4ac89d6c2795a5e5e04d93e
Vinnie Palazeti <vinniepalazeti@Vinnies-MacBook-Pro-2.local>
2021-02-27 10:37:38 -0500

    initial commit for bootstrap

---
+++ NN Inference/bootstrap/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="bootstraps",  # Replace with your own username
+    version="0.0.1",
+    author="Vinnie Palazeti",
+    author_email="vinnie.palazeti@gmail.com",
+    description="looking into bootstrap + NNs",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pandas',
+        'tensorflow',
+        'numpy',
+        'jupyter notebook'
+    ],
+    python_requires='>=3.6',
+)
